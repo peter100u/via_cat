8404cb106e50edd4b50d9d0265f630c40788aa94
ohla <1611641037@qq.com>
2024-05-28 17:05:01 +0800

    暂时关闭多余的功能

--- ui.R
+++ ui.R
@@ -14,18 +14,18 @@ bslib::page(
       div(
         align = "center",
         br(),
-        h2("数据可视化大全"),
-        p("无限精美的图表，满足你的一切视觉效果"),
+        h2("数据可视化"),
+        p("体验数据之美"),
         br(),
       )
     ),
-    # 第二部分：搜索框
-    fluidRow(
-      div(
-        align = "center",
-        textInput("search", label = NULL, placeholder = "开始您的搜索吧")
-      )
-    ),
+    # 第二部分：搜索框 （目前没太多的数据，不需要搜索）
+    # fluidRow(
+    #   div(
+    #     align = "center",
+    #     textInput("search", label = NULL, placeholder = "开始您的搜索吧")
+    #   )
+    # ),
 
     # 第三部分：图表作品区域
     fluidRow(
